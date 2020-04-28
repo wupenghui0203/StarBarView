ba6f7daa0ae05a2ba9e34914b838b9ce32f6840d
wupenghui0203 <32258076+wupenghui0203@users.noreply.github.com>
2020-04-29 00:54:49 +0800

    Update StarBarView.java
    44444

--- StarBarViews/app/src/main/java/www/ycode/xiaowu/com/starbarview/StarBarView.java
+++ StarBarViews/app/src/main/java/www/ycode/xiaowu/com/starbarview/StarBarView.java
@@ -32,7 +32,7 @@ public class StarBarView extends View {
     private float starRating;
     private Paint paint;
     private int mSpaceWidth;//星星间隔
-    private int mStarWidth;//星星宽度第五位 的玩儿
+    private int mStarWidth;//星星宽度第五位 的玩儿33333333
     private int mStarHeight;//星星高度
     private boolean isIndicator;//是否是一个指示器（用户无法进行更改）33
     private int mOrientation;
